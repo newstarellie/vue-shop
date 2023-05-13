4285e378974efd8d62650ed1608e8b861aef308c
newstarellie <90997488+newstarellie@users.noreply.github.com>
2023-05-13 05:50:42 +0000

    add router setting

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>商品列表</h1>
+    <!-- <ProductList :products="allProducts" @addToCart="addToCart" /> -->
+  </div>
+</template>
+
+<script>
+// import ProductList from '@/components/ProductList.vue';
+// import { mapGetters } from 'vuex';
+
+
+export default {
+  name: 'HomePage',
+  components: {
+    // ProductList,
+  },
+  created() {
+
+  },
+  computed: {
+    // ...mapGetters('products', ['allProducts']),
+  },
+  methods: {
+    // addToCart(product) {
+    //   // return this.$store.dispatch('cart/addToCart', product);
+    // },
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+h1 {
+  font-size: 2em;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+</style>
