243ce271ce3c5d93246031c8cb89aa5ca8d8be8c
newstarellie <newstarellie@gmail.com>
2023-05-17 16:23:49 +0800

    add setting in About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,15 @@
 <template>
-  <div class="home">
-    <h1>商品列表</h1>
+  <div class="about">
+    <h1>About Us</h1>
+    <p>
+      We are an online store dedicated to providing the best products and services to our customers. Our mission is to
+      create a seamless shopping experience for everyone.
+    </p>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'AboutPage',
-  components: {
-  },
-  created() {
-
-  },
-  computed: {
-
-  },
-  methods: {
-
-  }
 };
 </script>
-
-<style scoped></style>
