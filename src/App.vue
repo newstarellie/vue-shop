758fe972d4b6ceb4306eea8d4bc4449a5ec07144
newstarellie <newstarellie@gmail.com>
2023-05-17 16:34:01 +0800

    change layout setting of App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  {{ tryValue }}
-  <img alt="Vue logo" src="./assets/logo.png">
   <Navbar></Navbar>
   <router-view></router-view>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
-import { mapState } from 'vuex';
-
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
     Navbar
   },
   computed: {
-    ...mapState(['tryValue']),
   },
 
 }
@@ -26,13 +19,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
   background-color: $primary-color;
-
 }
 </style>
