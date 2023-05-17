375d72277507a79f3866d447c87ea4d1278ce6e5
newstarellie <newstarellie@gmail.com>
2023-05-17 15:53:01 +0800

    check mapState的功能

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
+  {{ tryValue }}
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  computed: {
+    ...mapState(['tryValue']),
+  },
+
 }
 </script>
 
