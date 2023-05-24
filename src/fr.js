a8a10da86b098845d03f830630aa10f5719d7b99
newstarellie <newstarellie@gmail.com>
2023-05-24 19:34:57 +0800

    add basic setting of firebase

---
+++ src/fr.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD81zKsJk1cPMyw2HUYnyQnTZ8lS6WgrrE",
+  authDomain: "vueshop-955e1.firebaseapp.com",
+  databaseURL: "https://vueshop-955e1-default-rtdb.firebaseio.com",
+  projectId: "vueshop-955e1",
+  storageBucket: "vueshop-955e1.appspot.com",
+  messagingSenderId: "833229035584",
+  appId: "1:833229035584:web:2a2f325e7a2787d81da7a5",
+  measurementId: "G-GCE28MKY7Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+
+function writeUserData(userId, name) {
+  console.log(userId);
+  // const db = getDatabase();
+  set(ref(database, userId), {
+    username: name,
+    // email: email,
+    // profile_picture: imageUrl
+  });
+}
+writeUserData('fd', 'name');
+
+
+export default {
+  database
+}
