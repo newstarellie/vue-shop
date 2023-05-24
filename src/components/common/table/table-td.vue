120289447c21b3c0011b5812fda1a190df5abc53
newstarellie <newstarellie@gmail.com>
2023-05-24 14:16:00 +0800

    add common\table\table-td.vue

---
+++ src/components/common/table/table-td.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <td v-if="dataForm == 'text'">
+    <span>{{ data }}</span>
+  </td>
+
+  <td v-else-if="dataForm == 'input'">
+    <input
+      type="number"
+      min="1"
+      :value="data"
+    />
+  </td>
+
+  <td v-else-if="dataForm == 'button'">
+    <button @click="this.$emit('click')">
+      {{ data }}
+    </button>
+  </td>
+</template>
+
+<script>
+export default {
+  name: 'tableTd',
+  props: {
+    data: {
+      type: String,
+      required: true
+    },
+    dataForm: {
+      type: String,
+      required: true
+    },
+  },
+};
+</script>
+
