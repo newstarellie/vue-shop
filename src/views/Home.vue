4e85f1a36559276adebf62aeda1326037db6e0e7
newstarellie <newstarellie@gmail.com>
2023-06-14 10:12:36 +0800

    add carousel basic setting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="home">
-    <h1>介紹</h1>
+
+    <carousel :items-to-show="1"
+      :transition="2000"
+      :infinite="true"
+      :autoplay="true">
+      <slide v-for="slide in 3"
+        :key="slide">
+        <div class="slide-content">
+          {{ slide }}
+        </div>
+      </slide>
+
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
   </div>
 </template>
 
 <script>
-
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
   name: 'HomePage',
   components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
   },
   created() {
 
@@ -19,5 +40,16 @@ export default {
   },
   methods: {
   }
-};
+}
 </script>
+<style scoped>
+.slide-content {
+  height: 100px;
+  width: 100px;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+}
+</style>
