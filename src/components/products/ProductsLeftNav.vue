0c825260e1918a5ee99d05defd952920ab97f866
newstarellie <newstarellie@gmail.com>
2023-06-15 18:54:27 +0800

    add basic setting of ProductsLeftNav.vue

---
+++ src/components/products/ProductsLeftNav.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="left-nav">
+    <div class="title">所有分类</div>
+    <div class="list-box">
+      <dl v-for="category in categories"
+        :key="category.title">
+        <dt>{{ category.title }}</dt>
+        <dd v-for="item in category.items"
+          :key="item">{{ item }}</dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductsLeftNav',
+  components: {
+  },
+  props: {
+
+  },
+  data() {
+    return {
+      categories: [
+        {
+          title: '奶粉輔食',
+          items: ['進口奶粉', '寶寶輔食', '營養品']
+        },
+        {
+          title: '兒童用品',
+          items: ['紙尿褲', '嬰兒濕巾', '嬰兒車', '嬰兒床', '兒童安全座椅']
+        },
+        {
+          title: '兒童早教',
+          items: ['兒童玩具', '早教書籍', '孕產育兒書']
+        },
+        {
+          title: '兒童服飾',
+          items: ['童裝', '童鞋', '寶寶配飾']
+        },
+        {
+          title: '孕媽專區',
+          items: ['孕婦裝', '孕婦護膚', '孕婦用品']
+        }
+      ]
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss">
+dl.accordion dt {
+  cursor: pointer;
+}
+
+dl.accordion dd {
+  display: none;
+}
+
+dl.accordion.open dd {
+  display: block;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,55 +1,21 @@
 <template>
   <div class="ProductsPage">
-    <div class="left-nav">
-      <div class="title">所有分类</div>
-      <div class="list-box">
-        <dl>
-          <dt class=""
-            off="">奶粉辅食</dt>
-          <dd style="display: block;"><a href="javascript:;">进口奶粉</a></dd>
-          <dd style="display: block;"><a href="javascript:;">宝宝辅食</a></dd>
-          <dd style="display: block;"><a href="javascript:;">营养品</a></dd>
-        </dl>
-        <dl>
-          <dt>儿童用品</dt>
-          <dd><a href="javascript:;">纸尿裤</a></dd>
-          <dd><a href="javascript:;">婴儿湿巾</a></dd>
-          <dd><a href="javascript:;">婴儿车</a></dd>
-          <dd><a href="javascript:;">婴儿床</a></dd>
-          <dd><a href="javascript:;">儿童安全座椅</a></dd>
-        </dl>
-        <dl>
-          <dt>儿童早教</dt>
-          <dd><a href="javascript:;">儿童玩具</a></dd>
-          <dd><a href="javascript:;">早教书籍</a></dd>
-          <dd><a href="javascript:;">孕产育儿书</a></dd>
-        </dl>
-        <dl>
-          <dt>儿童服饰</dt>
-          <dd><a href="javascript:;">童装</a></dd>
-          <dd><a href="javascript:;">童鞋</a></dd>
-          <dd><a href="javascript:;">宝宝配饰</a></dd>
-        </dl>
-        <dl>
-          <dt>孕妈专区</dt>
-          <dd><a href="javascript:;">孕妇装</a></dd>
-          <dd><a href="javascript:;">孕妇护肤</a></dd>
-          <dd><a href="javascript:;">孕妇用品</a></dd>
-        </dl>
-      </div>
-    </div>
+    <ProductsLeftNav></ProductsLeftNav>
+
     <ProductList :ProductList="productsList"></ProductList>
   </div>
 </template>
 
 <script>
 import ProductList from '@/components/products/ProductList.vue'
+import ProductsLeftNav from '@/components/products/ProductsLeftNav.vue'
 
 
 export default {
   name: 'ProductsPage',
   components: {
-    ProductList
+    ProductList,
+    ProductsLeftNav
   },
   data() {
     return {
