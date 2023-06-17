aac652aa07539371414cdfa7570a7f89c752bc18
newstarellie <newstarellie@gmail.com>
2023-06-17 09:50:58 +0800

    change Carousel setting

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="container">
     <carousel :items-to-show="1"
-      :transition="2000"
-      :infinite="true"
+      :transition="3000"
+      :wrapAround="true"
+      :snapAlign="center"
       :autoplay="true">
       <slide v-for="slide in bannerList"
         :key="slide">
         <div class="slide-content">
           <img :src="slide.path"
-            alt="slide-picture"
-            style="height: 100%; object-fit: cover;">
+            alt="slide-picture">
         </div>
-
       </slide>
 
       <template #addons>
@@ -25,9 +24,7 @@
 <script>
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
-// import banner1 from '@/assets/home/banner1';
-// import { banner1 } from "@/assets/home/";
-// import banner1 from '../assets/home/banner1.jpg';
+import banner1 from '../assets/home/banner1.jpg';
 import banner2 from '../assets/home/banner2.png';
 import banner3 from '../assets/home/banner3.jpg';
 
@@ -44,12 +41,10 @@ export default {
   data() {
     return {
       bannerList: [
-        // {
-        //   path: banner1,
-        //   index: 1,
-        //   width: '200px',
-
-        // },
+        {
+          path: banner1,
+          index: 1,
+        },
         {
           path: banner2,
           index: 2,
@@ -71,14 +66,14 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
 .slide-content {
-  height: 60vh;
+  height: 450px;
   width: 100%;
-  background-color: #eee;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background-color: $primary-color;
 
+  img {
+    height: 100%;
+  }
 }
 </style>
