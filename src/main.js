d538b8c8bf404dc3317cd4c2f5550e89895eef4f
newstarellie <newstarellie@gmail.com>
2023-06-19 12:49:57 +0800

    add @brackets/vue-toastification to project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue';
 import store from './store/index';
 import router from './router/index.js';
 
+// https://www.npmjs.com/package/@brackets/vue-toastification
+import Toast from "vue-toastification";
+
+
+
+
 // 创建 Vue 实例
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(Toast);
 
 app.mount('#app')
