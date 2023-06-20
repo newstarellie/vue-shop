a30f4950ec0d0575bc5245b431937085ae8ecb59
newstarellie <newstarellie@gmail.com>
2023-06-20 13:14:49 +0800

    Revert "change tableHead in cartItem\cartItemList.vue"
    This reverts commit 07057f19514cfd1337168bbc723714e947a33fad.

--- src/components/cartItem/cartItemList.vue
+++ src/components/cartItem/cartItemList.vue
@@ -1,45 +1,14 @@
 
 <template>
-  <div class="FloatBarHolder cart-table-th">
-    <div class="select-all">
-      <div class="cart-checkbox">
-        <input class="check-all check"
-          id="allChecked"
-          type="checkbox"
-          value="true">
-      </div>
-      <label class="th-inner">Select All</label>
-    </div>
-
-    <div v-for="item in tableHeaders"
-      class="th-inner"
-      :key="item.key">
-      {{ item.text }}
-    </div>
-  </div>
-
   <table>
-    <!-- <tableHead :table-column="tableColumn"></tableHead> -->
-
+    <tableHead :table-column="tableColumn"></tableHead>
     <tableBody :dataList="inCartProductsList">
     </tableBody>
   </table>
-  <div class="FloatBarHolder ">
-    <div class="select-all">
-      <div class="cart-checkbox">
-        <input class="check-all check"
-          id=""
-          name="select-all"
-          type="checkbox"
-          value="true">
-      </div>
-      <label>&nbsp;&nbsp;Selected <span class="Selected-pieces">3</span> items</label>
-    </div>
-  </div>
 </template>
 
 <script>
-// import tableHead from '@/components/common/table/table-head.vue';
+import tableHead from '@/components/common/table/table-head.vue';
 import tableBody from '@/components/cartItem/cartList-table-body.vue'
 export default {
   name: 'cartItemList',
@@ -50,19 +19,8 @@ export default {
     },
   },
   components: {
-    // tableHead,
+    tableHead,
     tableBody,
-  },
-  data() {
-    return {
-      tableHeaders: [
-        { key: "product", text: "Product" },
-        { key: "price", text: "Price" },
-        { key: "quantity", text: "Quantity" },
-        { key: "subtotal", text: "Subtotal" },
-        { key: "action", text: "Action" }
-      ]
-    };
   },
   computed: {
     tableColumn() {
@@ -71,20 +29,4 @@ export default {
   }
 };
 </script>
-<style lang="scss">
-.FloatBarHolder {
-  height: 50px;
-  line-height: 50px;
-  background: #f5f5f5;
-  display: flex;
 
-  .select-all {
-    display: flex;
-  }
-
-  .th-inner {
-    font-size: $font-size-base*2;
-    margin: 0.5rem;
-  }
-}
-</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,13 @@
 <template>
-  <div v-if="!cartIsEmpty">
-    <cartItemList :inCartProductsList="inCartProductsList"></cartItemList>
-    <p>總金額: {{ totalCartAmount }}</p>
-    <button @click="clearCart">清空購物車</button>
-    <button @click="checkout">結帳</button>
+  <div class="home">
+    <div v-if="!cartIsEmpty">
+      <cartItemList :inCartProductsList="inCartProductsList"></cartItemList>
+      <p>總金額: {{ totalCartAmount }}</p>
+      <button @click="clearCart">清空購物車</button>
+      <button @click="checkout">結帳</button>
+    </div>
+    <p v-else>購物車是空的</p>
   </div>
-  <p v-else>購物車是空的</p>
 </template>
 
 <script>
