917d8316b96e5bce3f3dc64820837f2c25ac7cf3
newstarellie <newstarellie@gmail.com>
2023-06-28 18:16:10 +0800

    intall Boostrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store/index';
 import router from './router/index.js';
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 // https://github.com/aFarkas/lazysizes
 import 'lazysizes';
