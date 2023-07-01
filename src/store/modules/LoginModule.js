1c62ee88fa7259ae2254216ad3db80c577abb661
newstarellie <newstarellie@gmail.com>
2023-07-01 17:43:21 +0800

    del old setting in LoginModule

--- src/store/modules/LoginModule.js
+++ src/store/modules/LoginModule.js
@@ -19,26 +19,7 @@ const actions = {
     commit('SET_REGISTER_PAGE_STATUS', status);
     router.push('/login');
   },
-  login({ commit, dispatch }, username) {
-    console.log(username)
-    commit('SET_LOGIN_STATUS', true)
-    commit('SET_USER', username)
-    dispatch('setUserNameToLocalStorage');
-  },
-  logout({ commit, dispatch }) {
-    // 登出邏輯
-    // 在這裡執行相關操作，如清除使用者資訊等
-    // 登出成功後觸發 mutation
-    commit('SET_LOGIN_STATUS', false)
-    commit('SET_USER', null)
-    dispatch('setUserNameToLocalStorage');
-  },
-  register({ commit, dispatch }, userData) {
-    console.log('註冊')
-    commit('SET_LOGIN_STATUS', true);
-    commit('SET_USER', userData.username);
-    dispatch('setUserNameToLocalStorage');
-  },
+
   setUserNameToLocalStorage({ dispatch }) {
     const payload = { name: 'username', data: JSON.stringify(state.user) };
     dispatch('setToLocalStorage', payload, { root: true });
